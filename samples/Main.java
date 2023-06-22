151ecb746a54c4e896b81741ee8a482ac0501942
Michał Guz <michal.guz.85@gmail.com>
2023-06-22 13:53:13 +0200

    Conversion and casting of types

---
+++ samples/Main.java
@@ -0,0 +1,8 @@
+import numbers.NumbersSamples;
+
+public class Main {
+    public static void main(String[] args) {
+
+        NumbersSamples.run();
+    }
+}
---
+++ samples/numbers/NumbersSamples.java
@@ -0,0 +1,29 @@
+package numbers;
+
+public class NumbersSamples {
+
+    public static void run() {
+        int weight = 5;
+        System.out.println("Weight is: " + weight);
+
+        float number = 5.5F;
+        float number2 = 10.5F;
+
+        double i = number + number2;
+
+        System.out.println(i);
+
+        int x = 1;
+        float y = 1.4F;
+        double z = 2.2;
+
+        float sum = x + y;
+        System.out.println(sum);
+
+        float v = x + y;// x+y
+        double v1 = x + z;// x+z
+        double v2 = y + z;// y+z
+
+
+    }
+}
