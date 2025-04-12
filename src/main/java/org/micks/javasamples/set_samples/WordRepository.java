8f91f691bdad5977a78f33a1711a4ac6dd4568f3
Michał Guz <michal.guz.85@gmail.com>
2025-04-12 13:20:11 +0200

    Corrected nulls

--- src/main/java/org/micks/javasamples/set_samples/WordRepository.java
+++ src/main/java/org/micks/javasamples/set_samples/WordRepository.java
@@ -1,41 +1,43 @@
 package org.micks.javasamples.set_samples;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 
 public class WordRepository {
 
-    private final Set<@NotNull String> words = new HashSet<>();
+    private final Set<String> words = new HashSet<>();
 
     public void addWord(String word) {
-        if (word != null) {
-            words.add(word.toLowerCase());
-        }
+        Optional.ofNullable(word)
+                .map(String::toLowerCase)
+                .ifPresent(words::add);
     }
 
     public void addWords(Collection<String> newWords) {
-        for (String word : newWords) {
-            if (word != null) {
-                words.add(word.toLowerCase());
-            }
-        }
+        Optional.ofNullable(newWords)
+                .ifPresent(list -> words.addAll(
+                        list.stream()
+                                .filter(Objects::nonNull)
+                                .map(String::toLowerCase)
+                                .collect(Collectors.toSet())
+                ));
     }
 
-    public List<@NotNull String> getWordsSorted() {
+    public List<String> getWordsSorted() {
         List<String> sorted = new ArrayList<>(words);
         Collections.sort(sorted);
         return sorted;
     }
 
-    public Set<@NotNull String> getCommonWords(Set<String> other) {
+    public Set<String> getCommonWords(Set<String> other) {
         return words.stream()
                 .filter(other::contains)
                 .collect(Collectors.toSet());
